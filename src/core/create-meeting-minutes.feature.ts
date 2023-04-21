289a5871c3bf57f0673baca15b7a05cf7a0cb771
Lucas Viana <oviana@outlook.com>
2023-04-21 15:01:01 -0300

    chore(minutes): Add alert with URL + update discord message

--- src/core/create-meeting-minutes.feature.ts
+++ src/core/create-meeting-minutes.feature.ts
@@ -6,6 +6,7 @@ import {
   SafeWrapper,
   SheetLogger,
   Student,
+  alert,
   getNamedValue,
   institutionalEmails,
   substituteVariablesInFile,
@@ -16,26 +17,21 @@ import { NamedRange } from '@utils/constants';
 import { getAllMembers, getPresentMembers } from '@utils/functions';
 
 const buildMeetingDiscordEmbeds = (project: Project, meetingStart: Date, attendees: Student[], clerk: string): DiscordEmbed[] => {
-  const fields: DiscordEmbed['fields'] = [
-    { name: 'Nome', value: project.name, inline: true },
-    { name: 'Edição', value: project.edition, inline: true },
-    { name: 'Ata', value: project.meetingMinutes.getUrl() },
-  ];
+  const fields: DiscordEmbed['fields'] = [];
 
-  fields.pushIf(attendees.length, { name: `Presentes (${attendees.length})`, value: attendees.toString() });
-  fields.pushIf(project.director || project.manager, { name: '', value: '' });
   fields.pushIf(project.manager, { name: 'Gerência', value: project.manager?.toString(), inline: true });
   fields.pushIf(clerk, { name: 'Redação', value: clerk, inline: true });
+  fields.pushIf(attendees.length, { name: `Presentes (${attendees.length})`, value: attendees.toString() });
 
   return [
     {
       title: 'Reunião de Projeto',
-      url: project.folder.getUrl(),
+      url: project.meetingMinutes.getUrl(),
       timestamp: meetingStart.toISOString(),
       fields,
       author: {
-        name: project.fullDepartmentName,
-        url: project.departmentFolder?.getUrl(),
+        name: project.toString(),
+        url: project.folder.getUrl(),
       },
     },
   ];
@@ -46,15 +42,16 @@ export const createMeetingMinutes = () =>
     (logger: SheetLogger) => {
       const project = Project.spreadsheetFactory();
       const clerk = getNamedValue(NamedRange.MeetingClerk);
+      const attendees = getPresentMembers();
 
-      if (!project.name || !project.edition || !project.manager || !project.department || !clerk) {
+      if (!project.name || !project.edition || !project.manager || !project.department || !clerk || !attendees.length) {
         throw Error(
-          'Estão faltando informações do projeto a ser aberto. São necessário pelo menos nome, edição, gerente, área e redator(a) da ata.',
+          'Estão faltando informações do projeto a ser aberto. São necessário pelo menos ' +
+            'nome, edição, gerente, área, redator(a) da ata e os membros presentes.',
         );
       }
       logger.log(DialogTitle.InProgress, `Execução iniciada para reunião com ${clerk} na redação.`);
 
-      const attendees = getPresentMembers();
       const minutesFile = project.createMeetingMinutes();
       const now = new Date();
       const variables: Record<MeetingVariable, string> = {
@@ -70,7 +67,10 @@ export const createMeetingMinutes = () =>
       minutesFile.setName(substituteVariablesInString(minutesFile.getName(), variables));
       substituteVariablesInFile(minutesFile, variables);
 
-      logger.log(`${DialogTitle.Success}`, `Ata criada com sucesso:\n${minutesFile.getUrl()}`);
+      const body = `Ata criada com sucesso:\n${minutesFile.getUrl()}`;
+
+      logger.log(DialogTitle.Success, body);
+      alert({ title: DialogTitle.Success, body });
       webhook.url && webhook.post({ embeds: buildMeetingDiscordEmbeds(project, now, attendees, clerk) });
     },
   );
